ea880f31c0a0247795f2fd50776704926b9a76ee
Justin Azoff <justin@bouncybouncy.net>
2018-08-17 08:35:11 -0400

    retab

--- xdp_autocutoff_kern.c
+++ xdp_autocutoff_kern.c
@@ -81,8 +81,8 @@ static __always_inline int get_dport(void *trans_data, void *data_end,
 }
 
 struct vlan_hdr {
-	__be16 h_vlan_TCI;
-	__be16 h_vlan_encapsulated_proto;
+    __be16 h_vlan_TCI;
+    __be16 h_vlan_encapsulated_proto;
 };
 
 //#define DEBUG 1
@@ -90,12 +90,12 @@ struct vlan_hdr {
 /* Only use this for debug output. Notice output from  bpf_trace_printk()
  * end-up in /sys/kernel/debug/tracing/trace_pipe
  */
-#define bpf_debug(fmt, ...)						\
-		({							\
-			char ____fmt[] = fmt;				\
-			bpf_trace_printk(____fmt, sizeof(____fmt),	\
-				     ##__VA_ARGS__);			\
-		})
+#define bpf_debug(fmt, ...)                     \
+        ({                          \
+            char ____fmt[] = fmt;               \
+            bpf_trace_printk(____fmt, sizeof(____fmt),  \
+                     ##__VA_ARGS__);            \
+        })
 #else
 #define bpf_debug(fmt, ...) { } while (0)
 #endif
@@ -106,50 +106,50 @@ struct vlan_hdr {
  */
 static __always_inline
 bool parse_eth(struct ethhdr *eth, void *data_end,
-	       u16 *eth_proto, u64 *l3_offset)
+           u16 *eth_proto, u64 *l3_offset)
 {
-	u16 eth_type;
-	u64 offset;
+    u16 eth_type;
+    u64 offset;
 
-	offset = sizeof(*eth);
-	if ((void *)eth + offset > data_end)
-		return false;
+    offset = sizeof(*eth);
+    if ((void *)eth + offset > data_end)
+        return false;
 
-	eth_type = eth->h_proto;
-	//bpf_debug("Debug: eth_type:0x%x\n", ntohs(eth_type));
+    eth_type = eth->h_proto;
+    //bpf_debug("Debug: eth_type:0x%x\n", ntohs(eth_type));
 
-	/* Skip non 802.3 Ethertypes */
-	if (unlikely(ntohs(eth_type) < ETH_P_802_3_MIN))
-		return false;
+    /* Skip non 802.3 Ethertypes */
+    if (unlikely(ntohs(eth_type) < ETH_P_802_3_MIN))
+        return false;
 
-	/* Handle VLAN tagged packet */
-	if (eth_type == htons(ETH_P_8021Q) || eth_type == htons(ETH_P_8021AD)) {
-		struct vlan_hdr *vlan_hdr;
+    /* Handle VLAN tagged packet */
+    if (eth_type == htons(ETH_P_8021Q) || eth_type == htons(ETH_P_8021AD)) {
+        struct vlan_hdr *vlan_hdr;
 
-		vlan_hdr = (void *)eth + offset;
-		offset += sizeof(*vlan_hdr);
-		if ((void *)eth + offset > data_end)
-			return false;
-		eth_type = vlan_hdr->h_vlan_encapsulated_proto;
-	}
-	/* TODO: Handle double VLAN tagged packet */
+        vlan_hdr = (void *)eth + offset;
+        offset += sizeof(*vlan_hdr);
+        if ((void *)eth + offset > data_end)
+            return false;
+        eth_type = vlan_hdr->h_vlan_encapsulated_proto;
+    }
+    /* TODO: Handle double VLAN tagged packet */
 
-	*eth_proto = ntohs(eth_type);
-	*l3_offset = offset;
-	return true;
+    *eth_proto = ntohs(eth_type);
+    *l3_offset = offset;
+    return true;
 }
 
 static __always_inline
 u32 parse_ipv4(struct xdp_md *ctx, u64 l3_offset)
 {
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data     = (void *)(long)ctx->data;
-	struct iphdr *iph = data + l3_offset;
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data     = (void *)(long)ctx->data;
+    struct iphdr *iph = data + l3_offset;
 
-	if (iph + 1 > data_end) {
-		bpf_debug("Invalid IPv4 packet: L3off:%llu\n", l3_offset);
-		return XDP_ABORTED;
-	}
+    if (iph + 1 > data_end) {
+        bpf_debug("Invalid IPv4 packet: L3off:%llu\n", l3_offset);
+        return XDP_ABORTED;
+    }
 
     int sport, dport;
     struct flowv4_keys tuple;
@@ -194,41 +194,41 @@ u32 parse_ipv4(struct xdp_md *ctx, u64 l3_offset)
 static __always_inline
 u32 handle_eth_protocol(struct xdp_md *ctx, u16 eth_proto, u64 l3_offset)
 {
-	switch (eth_proto) {
-	case ETH_P_IP:
-		return parse_ipv4(ctx, l3_offset);
-		break;
-	case ETH_P_IPV6: /* Not handler for IPv6 yet*/
-	case ETH_P_ARP:  /* Let OS handle ARP */
-		/* Fall-through */
-	default:
-		//bpf_debug("Not handling eth_proto:0x%x\n", eth_proto);
-		return XDP_PASS;
-	}
-	return XDP_PASS;
+    switch (eth_proto) {
+    case ETH_P_IP:
+        return parse_ipv4(ctx, l3_offset);
+        break;
+    case ETH_P_IPV6: /* Not handler for IPv6 yet*/
+    case ETH_P_ARP:  /* Let OS handle ARP */
+        /* Fall-through */
+    default:
+        //bpf_debug("Not handling eth_proto:0x%x\n", eth_proto);
+        return XDP_PASS;
+    }
+    return XDP_PASS;
 }
 
 
 SEC("xdp_ttl")
 int  xdp_ttl_program(struct xdp_md *ctx)
 {
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data     = (void *)(long)ctx->data;
-	struct ethhdr *eth = data;
-	u16 eth_proto = 0;
-	u64 l3_offset = 0;
-	u32 action;
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data     = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+    u16 eth_proto = 0;
+    u64 l3_offset = 0;
+    u32 action;
 
-	if (!(parse_eth(eth, data_end, &eth_proto, &l3_offset))) {
-		bpf_debug("Cannot parse L2: L3off:%llu proto:0x%x\n",
-			  l3_offset, eth_proto);
-		return XDP_PASS; /* Skip */
-	}
+    if (!(parse_eth(eth, data_end, &eth_proto, &l3_offset))) {
+        bpf_debug("Cannot parse L2: L3off:%llu proto:0x%x\n",
+              l3_offset, eth_proto);
+        return XDP_PASS; /* Skip */
+    }
 
-	//bpf_debug("Reached L3: L3off:%llu proto:0x%x\n", l3_offset, eth_proto);
+    //bpf_debug("Reached L3: L3off:%llu proto:0x%x\n", l3_offset, eth_proto);
 
-	action = handle_eth_protocol(ctx, eth_proto, l3_offset);
-	return action;
+    action = handle_eth_protocol(ctx, eth_proto, l3_offset);
+    return action;
 }
 
 char _license[] SEC("license") = "GPL";
--- xdp_autocutoff_user.c
+++ xdp_autocutoff_user.c
@@ -25,50 +25,50 @@ static int ifindex = -1;
 
 static void int_exit(int sig)
 {
-	fprintf(stderr, "Interrupted: Removing XDP program on ifindex:%d\n",
-		ifindex);
-	if (ifindex > -1)
-		set_link_xdp_fd(ifindex, -1, 0);
-	exit(0);
+    fprintf(stderr, "Interrupted: Removing XDP program on ifindex:%d\n",
+        ifindex);
+    if (ifindex > -1)
+        set_link_xdp_fd(ifindex, -1, 0);
+    exit(0);
 }
 
 static const struct option long_options[] = {
-	{"help",	no_argument,		NULL, 'h' },
-	{"ifname",	required_argument,	NULL, 'i' },
-	{0, 0, NULL,  0 }
+    {"help",    no_argument,        NULL, 'h' },
+    {"ifname",  required_argument,  NULL, 'i' },
+    {0, 0, NULL,  0 }
 };
 
 /* Exit return codes */
-#define	EXIT_OK			0
-#define EXIT_FAIL		1
-#define EXIT_FAIL_OPTION	2
-#define EXIT_FAIL_XDP		3
+#define EXIT_OK         0
+#define EXIT_FAIL       1
+#define EXIT_FAIL_OPTION    2
+#define EXIT_FAIL_XDP       3
 
 static void usage(char *argv[])
 {
-	int i;
-	printf("\nDOCUMENTATION:\n%s\n", __doc__);
-	printf("\n");
-	printf(" Usage: %s (options-see-below)\n",
-	       argv[0]);
-	printf(" Listing options:\n");
-	for (i = 0; long_options[i].name != 0; i++) {
-		printf(" --%-12s", long_options[i].name);
-		if (long_options[i].flag != NULL)
-			printf(" flag (internal value:%d)",
-			       *long_options[i].flag);
-		else
-			printf(" short-option: -%c",
-			       long_options[i].val);
-		printf("\n");
-	}
-	printf("\n");
+    int i;
+    printf("\nDOCUMENTATION:\n%s\n", __doc__);
+    printf("\n");
+    printf(" Usage: %s (options-see-below)\n",
+           argv[0]);
+    printf(" Listing options:\n");
+    for (i = 0; long_options[i].name != 0; i++) {
+        printf(" --%-12s", long_options[i].name);
+        if (long_options[i].flag != NULL)
+            printf(" flag (internal value:%d)",
+                   *long_options[i].flag);
+        else
+            printf(" short-option: -%c",
+                   long_options[i].val);
+        printf("\n");
+    }
+    printf("\n");
 }
 
-#define MAX_KEYS	256
+#define MAX_KEYS    256
 
 struct ttl_stats {
-	__u64 data[MAX_KEYS];
+    __u64 data[MAX_KEYS];
 };
 
 struct flowv4_keys {
@@ -123,124 +123,124 @@ static bool stats_collect(struct ttl_stats *record)
 
         }
         key = next_key;
-	}
+    }
     printf("Flows: total=%d expired=%d\n", flows_total, flows_expired);
-	return false;
+    return false;
 }
 
 static void stats_print_headers(void)
 {
-	static unsigned int i;
+    static unsigned int i;
 #define DEBUG 1
 #ifdef  DEBUG
-	{
-	int debug_notice_interval = 3;
-	char msg[] =
-		"\nDebug outout avail via:\n"
-		" sudo cat /sys/kernel/debug/tracing/trace_pipe\n\n";
-	printf(msg, debug_notice_interval);
-	}
+    {
+    int debug_notice_interval = 3;
+    char msg[] =
+        "\nDebug outout avail via:\n"
+        " sudo cat /sys/kernel/debug/tracing/trace_pipe\n\n";
+    printf(msg, debug_notice_interval);
+    }
 #endif
-	i++;
-	printf("Stats: %d\n", i);
+    i++;
+    printf("Stats: %d\n", i);
 }
 
 static void stats_print(struct ttl_stats *record)
 {
-	const unsigned int nr_keys = MAX_KEYS;
-	__u64 count;
-	__u32 ttl;
+    const unsigned int nr_keys = MAX_KEYS;
+    __u64 count;
+    __u32 ttl;
 
-	/* clear screen */
-	printf("\033[2J");
+    /* clear screen */
+    printf("\033[2J");
 
-	stats_print_headers();
-	for (ttl = 0; ttl < nr_keys; ttl++) {
-		count = record->data[ttl];
-		if (count)
-			printf("TTL: %3d count:%llu\n", ttl, count);
-	}
+    stats_print_headers();
+    for (ttl = 0; ttl < nr_keys; ttl++) {
+        count = record->data[ttl];
+        if (count)
+            printf("TTL: %3d count:%llu\n", ttl, count);
+    }
 }
 
 static void stats_poll(int interval)
 {
-	struct ttl_stats record;
+    struct ttl_stats record;
 
-	while (1) {
-		memset(&record, 0, sizeof(record));
+    while (1) {
+        memset(&record, 0, sizeof(record));
 
-		if (stats_collect(&record))
-			stats_print(&record);
+        if (stats_collect(&record))
+            stats_print(&record);
 
-		sleep(interval);
-	}
+        sleep(interval);
+    }
 }
 
 int main(int argc, char **argv)
 {
-	struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
-	char filename[256];
-	int longindex = 0;
-	int opt;
-	char *ifname=NULL;
+    struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
+    char filename[256];
+    int longindex = 0;
+    int opt;
+    char *ifname=NULL;
 
-	snprintf(filename, sizeof(filename), "%s_kern.o", argv[0]);
+    snprintf(filename, sizeof(filename), "%s_kern.o", argv[0]);
 
-	/* Parse commands line args */
-	while ((opt = getopt_long(argc, argv, "hi:",
-				  long_options, &longindex)) != -1) {
-		switch (opt) {
-		case 'i':
-			ifname = optarg;
-			break;
-		case 'h':
-		default:
-			usage(argv);
-			return EXIT_FAIL_OPTION;
-		}
-	}
-	/* Required options */
-	if (ifname==NULL) {
-		printf("**Error**: required option --ifname missing");
-		usage(argv);
-		return EXIT_FAIL_OPTION;
-	}
+    /* Parse commands line args */
+    while ((opt = getopt_long(argc, argv, "hi:",
+                  long_options, &longindex)) != -1) {
+        switch (opt) {
+        case 'i':
+            ifname = optarg;
+            break;
+        case 'h':
+        default:
+            usage(argv);
+            return EXIT_FAIL_OPTION;
+        }
+    }
+    /* Required options */
+    if (ifname==NULL) {
+        printf("**Error**: required option --ifname missing");
+        usage(argv);
+        return EXIT_FAIL_OPTION;
+    }
     if((ifindex=if_nametoindex(ifname)) == 0) {
         perror("Can't find interface");
         return EXIT_FAIL_OPTION;
     }
-	/* Required options */
-	if (ifindex == -1) {
-		printf("**Error**: required option --ifindex missing");
-		usage(argv);
-		return EXIT_FAIL_OPTION;
-	}
+    /* Required options */
+    if (ifindex == -1) {
+        printf("**Error**: required option --ifindex missing");
+        usage(argv);
+        return EXIT_FAIL_OPTION;
+    }
 
-	/* Increase resource limits */
-	if (setrlimit(RLIMIT_MEMLOCK, &r)) {
-		perror("setrlimit(RLIMIT_MEMLOCK, RLIM_INFINITY)");
-		return 1;
-	}
+    /* Increase resource limits */
+    if (setrlimit(RLIMIT_MEMLOCK, &r)) {
+        perror("setrlimit(RLIMIT_MEMLOCK, RLIM_INFINITY)");
+        return 1;
+    }
 
-	if (load_bpf_file(filename)) {
-		printf("%s", bpf_log_buf);
-		return 1;
-	}
+    if (load_bpf_file(filename)) {
+        printf("%s", bpf_log_buf);
+        return 1;
+    }
 
-	if (!prog_fd[0]) {
-		printf("load_bpf_file: %s\n", strerror(errno));
-		return 1;
-	}
+    if (!prog_fd[0]) {
+        printf("load_bpf_file: %s\n", strerror(errno));
+        return 1;
+    }
 
-	/* Remove XDP program when program is interrupted */
-	signal(SIGINT, int_exit);
+    /* Remove XDP program when program is interrupted */
+    signal(SIGINT, int_exit);
 
-	if (set_link_xdp_fd(ifindex, prog_fd[0], 0) < 0) {
-		printf("link set xdp fd failed\n");
-		return EXIT_FAIL_XDP;
-	}
+    if (set_link_xdp_fd(ifindex, prog_fd[0], 0) < 0) {
+        printf("link set xdp fd failed\n");
+        return EXIT_FAIL_XDP;
+    }
 
-	stats_poll(1);
+    stats_poll(1);
 
-	return EXIT_OK;
+    return EXIT_OK;
 }
