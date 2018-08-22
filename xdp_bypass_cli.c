4a4e939122e64b54b659f6b00fe0b00fa659be29
Justin Azoff <justin@bouncybouncy.net>
2018-08-22 18:38:11 -0400

    un hard code

--- xdp_bypass_cli.c
+++ xdp_bypass_cli.c
@@ -1,10 +1,20 @@
 #include "xdp_bypass_lib.h"
 
-int main(int argc, char **arvc) {
+int main(int argc, char **argv) {
     int fd = 0;
     int res;
+    if(argc < 5) {
+        fprintf(stderr, "Usage: %s src sport dst dport\n", argv[0]);
+        return 1;
+    }
     fd = open_bpf_map(PIN_PATH "/flow_table_v4");
-    res = xdp_bypass_v4(fd, "192.168.2.1", 5001, "192.168.2.22", 5005);
+
+    char *src = argv[1];
+    int sport = atoi(argv[2]);
+    char *dst = argv[3];
+    int dport = atoi(argv[4]);
+
+    res = xdp_bypass_v4(fd, src, sport, dst, dport);
     printf("Res: %d\n", res);
     return res;
 }
